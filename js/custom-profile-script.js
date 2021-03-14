c99ee9fc894465d218c85dc2a29019a36a675edd
felipewesley <fe.wesleybasso@gmail.com>
2021-03-14 03:10:23 -0300

    Top small box with some user informations

--- js/custom-profile-script.js
+++ js/custom-profile-script.js
@@ -13,6 +13,8 @@ window.onload = function() {
     this.onselectstart = () => false;
     this.onresize = (event) => console.log("Resized to ", event.target.innerWidth, "px");
 
+    const GITHUB_PROFILE = "https://github.com/felipewesley";
+
     // Toggle theme buttons
     let btns_toggle_theme = document.getElementsByName("i-toggle-theme");
     btns_toggle_theme.forEach(e => {
@@ -26,6 +28,22 @@ window.onload = function() {
                 element.className += new_theme == "light" ? "off" : "on";
                 element.setAttribute("aria-details", new_theme);
             })
+        }
+    })
+
+    // Redirect to profile
+    let btn_see_profile = document.getElementsByName("btn-see-profile");
+    btn_see_profile.forEach(e => {
+        e.onclick = () => window.open(GITHUB_PROFILE, "_blank");
+    });
+
+    // Ask if really want to go to github profile
+    let btn_github = document.getElementsByName("btn-to-github");
+    btn_github.forEach(e => {
+        e.onclick = () => {
+            let msg = "Realmente deseja ir para o perfil de Felipe?"
+            if (confirm(msg))
+                window.open(GITHUB_PROFILE, "_blank");
         }
     })
 
